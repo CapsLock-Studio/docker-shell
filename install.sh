4a743964c49f3cbb03799a5b41aa749b5cdfea04
michael34435 <michael34435@gmail.com>
2016-12-02 02:33:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
-ln -s $(pwd)/docker-shell /usr/local/bin/docker-shell
+install=/usr/local/bin/docker-shell
+
+
+if [[ -h $install ]] || [[ ! -f $install ]]; then
+  rm -rf $install
+  ln -s $(pwd)/docker-shell $install
+  echo "\033[0;32mSuccessfully installed!\033[0m"
+elif [[ -f $install ]]; then
+  echo "\033[0;31mI can not install it because of docker-shell in your /usr/local/bin is a regular file.\033[0m"
+  exit 1
+fi
